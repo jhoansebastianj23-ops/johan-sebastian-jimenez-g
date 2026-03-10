ccd4cd9a7b2ecf3b171b137bba38585b6a9b5aa9
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-03-09 23:13:38 -0500

    Update Prestamo.java

--- trabajo carlos/Prestamo.java
+++ trabajo carlos/Prestamo.java
@@ -1,13 +1,1 @@
-public class Prestamo {
 
-    Libro libro;
-    Usuario usuario;
-
-    public Prestamo(Libro libro, Usuario usuario) {
-
-        this.libro = libro;
-        this.usuario = usuario;
-
-    }
-
-}
