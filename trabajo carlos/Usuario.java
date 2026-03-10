4dd3e8eb173d8c7a3942f16dc45bde442c4fcdff
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-03-09 23:14:02 -0500

    Update Usuario.java

--- trabajo carlos/Usuario.java
+++ trabajo carlos/Usuario.java
@@ -1,17 +1,1 @@
-public class Usuario {
 
-    String documento;
-    String nombre;
-    String rol;
-    String password;
-
-    public Usuario(String documento, String nombre, String rol, String password) {
-
-        this.documento = documento;
-        this.nombre = nombre;
-        this.rol = rol;
-        this.password = password;
-
-    }
-
-}
