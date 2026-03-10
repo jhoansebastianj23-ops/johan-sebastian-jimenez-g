bd005bdcabe51a830725e580e77cf7ecfa4af544
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-03-09 23:46:37 -0500

    modificasion
    métodos para obtener los datos del libro, y método mostrar el loblro

--- trabajo carlos/Libro.java
+++ trabajo carlos/Libro.java
@@ -12,4 +12,39 @@ public class Libro {
         this.autor = autor;
         this.anio = anio;
         this.disponible = true;
+           }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public void mostrarLibro() {
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año: " + anio);
+        System.out.println("Estado: " + (disponible ? "Disponible" : "Prestado"));
+    }
+}
+
 
