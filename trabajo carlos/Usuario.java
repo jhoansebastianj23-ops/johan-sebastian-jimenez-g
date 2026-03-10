55953d47c981dd7cbf27d79e4607fe01994d7689
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-03-09 23:57:58 -0500

    Creación de la clase  public class usuario
    puse atributos del usuario, el constructor, guardar los datos, métodos GET, método mostrar usuario.

--- trabajo carlos/Usuario.java
+++ trabajo carlos/Usuario.java
@@ -1,1 +1,31 @@
+public class Usuario {
+
+    private String documento;
+    private String nombre;
+    private String tipoUsuario; // Administrador, Bibliotecario o Lector
+
+    public Usuario(String documento, String nombre, String tipoUsuario) {
+        this.documento = documento;
+        this.nombre = nombre;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void mostrarUsuario() {
+        System.out.println("Documento: " + documento);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Tipo de usuario: " + tipoUsuario);
+    }
+}
 
