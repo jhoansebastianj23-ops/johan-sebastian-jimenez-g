d85ffad450ff9c419d5d5e412f0eb4e3024d34a3
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-03-09 23:31:26 -0500

    creación de la clase libro
    declare los atributos del libro, la creación del constructor, asigne los valores,

--- trabajo carlos/Libro.java
+++ trabajo carlos/Libro.java
@@ -1,1 +1,15 @@
+public class Libro {
+
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anio;
+    private boolean disponible;
+
+    public Libro(String isbn, String titulo, String autor, int anio) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.disponible = true;
 
