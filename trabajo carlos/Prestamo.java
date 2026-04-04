987a27da55a844a3a4a6e738f4d84f54da8afa77
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-04-03 23:50:41 -0500

    se creo molde para guardar
    Representa un préstamo
     Une un libro con un usuario
    Guarda quién tiene qué libro

--- trabajo carlos/Prestamo.java
+++ trabajo carlos/Prestamo.java
@@ -1,1 +1,9 @@
+public class Prestamo {
+    String isbn;
+    String documento;
 
+    public Prestamo(String isbn, String documento) {
+        this.isbn = isbn;
+        this.documento = documento;
+    }
+}
