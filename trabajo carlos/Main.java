20fe3d2cf1ab44ef4b21ae2d484a061e8d28bc83
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-04-05 18:31:28 -0500

    Update Main.java
    “Inicializa el sistema de biblioteca y solicita credenciales para el inicio de sesión.”

--- trabajo carlos/Main.java
+++ trabajo carlos/Main.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Biblioteca b = new Biblioteca();
+        Scanner sc = new Scanner(System.in);
+
+        // Usuario inicial
+        Usuario admin = new Usuario("1", "Admin", "Administrador", "123");
+        b.registrarUsuario(admin);
+
+        System.out.println("=== LOGIN ===");
+        System.out.print("Documento: ");
+        String doc = sc.nextLine();
+        System.out.print("Password: ");
+        String pass = sc.nextLine();
