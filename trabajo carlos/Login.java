af0e030f68ced0bda61c81568cce9ddfaa58c7e9
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-04-04 23:14:17 -0500

    creacion
    Verifica permisos (solo admin o bibliotecario)
     Verifica que el libro y usuario existan
    Verifica que el libro esté disponible
     Verifica que no supere el límite de préstamos
    Guarda el préstamo y marca el libro como prestado

--- trabajo carlos/Login.java
+++ trabajo carlos/Login.java
@@ -51,3 +51,40 @@ public void registrarUsuario(Usuario u) {
     public void listarUsuarios() {
         for (Usuario u : usuarios) u.mostrar();
     }
+public void prestar(String isbn, String doc, Usuario admin) {
+        if (!(admin.tipo.equals("Administrador") || admin.tipo.equals("Bibliotecario"))) {
+            System.out.println("No tienes permiso");
+            return;
+        }
+
+        Libro l = buscarLibro(isbn);
+        Usuario u = buscarUsuario(doc);
+
+        if (l == null || u == null) {
+            System.out.println("Datos incorrectos");
+            return;
+        }
+
+        if (!l.disponible) {
+            System.out.println("Libro no disponible");
+            return;
+        }
+
+        long cantidad = prestamos.stream().filter(p -> p.documento.equals(doc)).count();
+
+        if (cantidad >= maxPrestamos) {
+            System.out.println("Máximo de préstamos alcanzado");
+            return;
+        }
+
+        prestamos.add(new Prestamo(isbn, doc));
+        l.disponible = false;
+    }
+
+    public void devolver(String isbn) {
+        prestamos.removeIf(p -> p.isbn.equals(isbn));
+
+        Libro l = buscarLibro(isbn);
+        if (l != null) l.disponible = true;
+    }
+}
