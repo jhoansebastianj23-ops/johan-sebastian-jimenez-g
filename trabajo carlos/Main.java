54b7faa52407aed661c4bc8ffc12e9dcab1e0e48
johan sebastian jimenez <jhoansebastianj23@gmail.com>
2026-04-05 19:13:52 -0500

    Implement user login verification
    Busca el usuario
    Valida login
    Si está bien → muestra el menú
    Permite elegir opciones

--- trabajo carlos/Main.java
+++ trabajo carlos/Main.java
@@ -15,3 +15,22 @@ public class Main {
         String doc = sc.nextLine();
         System.out.print("Password: ");
         String pass = sc.nextLine();
+Usuario log = b.buscarUsuario(doc);
+
+        if (log == null || !log.password.equals(pass)) {
+            System.out.println("Error de login");
+            return;
+        }
+
+        int op;
+        do {
+            System.out.println("\n1. Registrar libro");
+            System.out.println("2. Listar libros");
+            System.out.println("3. Registrar usuario");
+            System.out.println("4. Listar usuarios");
+            System.out.println("5. Prestar libro");
+            System.out.println("6. Devolver libro");
+            System.out.println("0. Salir");
+
+            op = sc.nextInt();
+            sc.nextLine();
